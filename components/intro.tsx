ad58559c23f02365e7f0286d63887243cfef0a8d
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-10-26 00:35:34 +0700

    Add Intro Section

---
+++ components/intro.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaFacebookSquare, FaGithubSquare } from "react-icons/fa";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src="https://source.unsplash.com/random"
+              alt="Minh Duy Dev"
+              width={192}
+              height={192}
+              quality={95}
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I&apos;m Minh Duy.</span> I&apos;m a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">1 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js) & Express.js (NestJS)</span>
+        .
+      </motion.p>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1.5 transition" />
+        </Link>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-70 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/duy-nguy%E1%BB%85n-vi%E1%BA%BFt-minh-212271235/"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.2rem] rounded-full cursor-pointer outline-none focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition border border-black/10"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/minhduydev"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.3rem] rounded-full cursor-pointer outline-none focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition border border-black/10"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+        <a
+          href="https://facebook.com/nvmd.dev"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.3rem] rounded-full cursor-pointer outline-none focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition border border-black/10"
+          target="_blank"
+        >
+          <FaFacebookSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
